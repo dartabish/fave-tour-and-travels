dafbccb49b82af08c7ab9aa5528f763acd639559
dartabish <dartabish01@gmail.com>
2024-01-25 00:05:01 +0530

    added nav and hero section

---
+++ script.js
@@ -0,0 +1,43 @@
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+  let favHeight;
+  var scrollPosition = window.scrollY;
+  var screenWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+  if (screenWidth <= 998) {
+    favHeight = 100;
+  } else {
+    favHeight = 500;
+  }
+  if (scrollPosition > favHeight) {
+    navbar.style.background = 'darkslategrey ';
+  } else {
+    navbar.style.backgroundColor = 'transparent';
+  }
+});
+
+var swiper = new Swiper('.destinations-swiper', {
+  speed: 800,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    998: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
